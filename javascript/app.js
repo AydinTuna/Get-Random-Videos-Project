c6886a4234e65ac6d1994c8b768ef31a58bbf378
aydintuna <aydinbahadirtuna@gmail.com>
2022-04-28 12:56:45 +0300

    v0.5

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+const giris = document.getElementsByClassName("giris");
+const video = document.getElementsByClassName("video");
+
+function videoBlur() {
+    console.log("meraba");
+}
+
+video.addEventListener("load", videoBlur);
+
+
+
