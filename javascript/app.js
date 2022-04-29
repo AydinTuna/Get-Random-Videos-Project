01902f05540e5cbb3971ee68a4ac29615deff9da
aydintuna <aydinbahadirtuna@gmail.com>
2022-04-29 03:50:24 +0300

    v0.7

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,32 @@
 const video = document.querySelector(".video");
 const giris = document.querySelector(".giris");
-const rastgeleButton = document.querySelector("body > div.header > div.video > div > button")
+const form = document.querySelector("form");
+const rastgeleButton = document.querySelector("body > div.header > div.video > div > button");
+const email = document.querySelector("input[type=email]:nth-child(1)");
+const sifre = document.querySelector("input[type=sifre]:nth-child(2)");
+const basarili = document.querySelector(".basarili");
+
+const data = {
+    email : "aydinbahadirtuna@gmail.com",
+    sifre : "1234567890"
+}
 
 function videoBlur() {
     video.style.filter = "blur(5px)";
     rastgeleButton.setAttribute("disabled", "disabled");
 }
 
-document.addEventListener("load", videoBlur());
+function girisKontrol() {
+    if (email.value === data.email && sifre.value === data.sifre) {
+        basarili.style.display = "flex";
+    }
+    else basarili.style.display = "none";
 
+    
+}
+
+document.addEventListener("load", videoBlur());
+form.addEventListener("submit", girisKontrol());
 
 
 
