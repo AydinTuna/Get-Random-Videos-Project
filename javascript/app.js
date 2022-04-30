a0d07ecb6a8ffdf3c9f2e7b0d0308f3a7ec02fef
AydinTuna <tunaaydin6161@gmail.com>
2022-04-30 17:05:54 +0300

    v1.0

--- javascript/app.js
+++ javascript/app.js
@@ -2,13 +2,42 @@ const video = document.querySelector(".video");
 const giris = document.querySelector(".giris");
 const form = document.querySelector("form");
 const rastgeleButton = document.querySelector("body > div.header > div.video > div > button");
-const email = document.querySelector("input[type=email]:nth-child(1)");
-const sifre = document.querySelector("input[type=sifre]:nth-child(2)");
+const email = document.querySelector("input[type=email]");
+const sifre = document.querySelector("input[type=password]");
 const basarili = document.querySelector(".basarili");
-
+const basariliKapat = document.querySelector(".basarili i");
+const girisButton = document.querySelector("form .button");
 const data = {
     email : "aydinbahadirtuna@gmail.com",
-    sifre : "1234567890"
+    sifre : "123"
+}
+
+EventListeners();
+
+function EventListeners() {
+    giris.addEventListener("load", videoBlur());
+    form.addEventListener("submit", girisKontrol);
+    basariliKapat.addEventListener("click", uyariKapat);
+}
+
+function disabled() {
+    girisButton.setAttribute("disabled", "disabled");
+    email.setAttribute("disabled", "disabled");
+    sifre.setAttribute("disabled", "disabled");
+}
+
+function uyariKapat() {
+    basarili.style.display = "none";
+    giris.style.filter = "blur(5px)";
+    video.style.filter = "blur(0px)";
+    rastgeleButton.removeAttribute("disabled");
+    disabled();
+}
+
+function uyariKapatTimer() {
+    setTimeout(function() {
+        uyariKapat();
+    }, 3000);
 }
 
 function videoBlur() {
@@ -16,17 +45,22 @@ function videoBlur() {
     rastgeleButton.setAttribute("disabled", "disabled");
 }
 
-function girisKontrol() {
+function girisKontrol(e) {
     if (email.value === data.email && sifre.value === data.sifre) {
+        giris.style.filter = "blur(5px)";
         basarili.style.display = "flex";
+        uyariKapatTimer();
+        e.preventDefault();
     }
-    else basarili.style.display = "none";
+    else {
+        basarili.style.display = "none";
+        e.preventDefault();
+    } 
 
-    
 }
 
-document.addEventListener("load", videoBlur());
-form.addEventListener("submit", girisKontrol());
+
+
 
 
 
