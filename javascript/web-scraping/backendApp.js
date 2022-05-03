3f2b5980b70baf03dbe898c1749814995a7a9b85
AydinTuna <tunaaydin6161@gmail.com>
2022-05-04 02:32:41 +0300

    v1.5

--- javascript/web-scraping/backendApp.js
+++ javascript/web-scraping/backendApp.js
@@ -2,10 +2,10 @@
 EventListeners();
 
 function EventListeners() {
-    rastgeleButton.addEventListener("click", getRandomVideo);
+    rastgeleButton.addEventListener("click", openYT);
 }
 
-function getRandomVideo() {
+function openYT() {
     rastgeleButton.setAttribute("href", "https://www.youtube.com/playlist?list=WL");
     rastgeleButton.setAttribute("target", "_blank");
 }
---
+++ javascript/web-scraping/index.js
@@ -0,0 +1,3 @@
+const axios = require("axios");
+
+const url = "https://www.youtube.com/playlist?list=WL";
